967018689b373878f3b83443b5dc8d58a7702810
Cleedee <claudiotorcato@gmail.com>
2026-03-13 12:00:33 -0300

    Mock em Roller.d6

--- tests/test_pc_elf.py
+++ tests/test_pc_elf.py
@@ -1,8 +1,12 @@
 import unittest
+from unittest.mock import Mock
 
 from notecli.entities import PlayerCharacter
 from notecli.entities.magic import factory_magic
+from notecli.dice import Roller
 from notecli import tables
+
+Roller = Mock()
 
 class TestElf(unittest.TestCase):
 
@@ -12,11 +16,13 @@ class TestElf(unittest.TestCase):
             torches=10,
             occupation="Noble"
         )
-        ancestry = tables.ANCESTRIES[6] # Elf
-        ancestry.apply(self.pc)
 
     def test_use_heal(self):
+        ancestry = tables.ANCESTRIES[6] # Elf
+        Roller.d6.return_value = 1
+        ancestry.apply(self.pc)
         self.pc.use_magic(0)
+        self.assertEqual(self.pc.ancestry, "Elfo")
         self.assertEqual(self.pc.health_points, 16, "Está no máximo de seus pontos de vida")
 
 if __name__ == '__main__':
